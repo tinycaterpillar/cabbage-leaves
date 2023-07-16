e1f988c91306d1f3fae7bbd872626c801e0e5931
Jinik Hwang <jinik990305@gmail.com>
2023-07-16 17:58:18 +0900

    backup

--- 00test.py
+++ 00test.py
@@ -1,8 +1,6 @@
 import sys
 import os.path
 
-if os.path.isfile('input.txt'):
-    sys.stdin = open('input.txt')
-    sys.stdout = open('output.txt', 'w')
 input = sys.stdin.readline
 
+print(input())
---
+++ 01test.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl '\n' // don't use when you cover interactive problem
+#define all(v) (v).begin(), (v).end()
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    
+
+    return 0;
+}
